bddf77ce199e1e953bcb2a561f9ff51d219c0fdf
Krantikumar4211 <krantikumar4211@gmail.com>
2025-12-04 23:06:22 +0530

    Homework

---
+++ Data/name.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	age1, age2, age3 := Age()
+
+	fmt.Println("1st Member Age :", age1)
+	fmt.Println("2nd Member Age :", age2)
+	fmt.Println("3rd Member Age :", age3)
+}
