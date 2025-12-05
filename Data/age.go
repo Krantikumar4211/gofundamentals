2716268a727f23ec80f5d3423b7768875a2c4406
Krantikumar4211 <krantikumar4211@gmail.com>
2025-12-05 13:33:52 +0530

    HW

--- Data/age.go
+++ Data/age.go
@@ -3,9 +3,6 @@ package main
 import "fmt"
 
 func Age() {
-	var age1 int
-	var age2 int
-	var age3 int
 
 	fmt.Println("Enter 1st Member Age")
 	fmt.Scanln(&age1)
@@ -16,11 +13,4 @@ func Age() {
 	fmt.Println("Enter 3st Member Age")
 	fmt.Scanln(&age3)
 
-	fmt.Println("1st Member Age :", age1)
-	fmt.Println("2st Member Age :", age2)
-	fmt.Println("3st Member Age :", age3)
-}
-func main() {
-
-	Age()
 }
--- Data/name.go
+++ Data/name.go
@@ -2,10 +2,26 @@ package main
 
 import "fmt"
 
+var age1, age2, age3 int
+var name1, name2, name3 string
+
 func main() {
-	age1, age2, age3 := Age()
+	Age()
 
 	fmt.Println("1st Member Age :", age1)
 	fmt.Println("2nd Member Age :", age2)
 	fmt.Println("3rd Member Age :", age3)
+
+	fmt.Println("Enter 1st member Name: ")
+	fmt.Scanln(&name1)
+
+	fmt.Println("Enter 2st member Name: ")
+	fmt.Scanln(&name2)
+
+	fmt.Println("Enter 3st member Name: ")
+	fmt.Scanln(&name3)
+
+	fmt.Println("The First Member name is ", name1, " And Age is :", age1)
+	fmt.Println("The Second Member name is ", name2, " And Age is :", age2)
+	fmt.Println("The Third Member name is ", name3, " And Age is :", age3)
 }
